30ce5df83442a4e1ecf6a7c463aff6b294bfa83d
Davidguar83 <davidrguardiola@hotmail.es>
2021-01-31 19:53:28 +0100

    primer

---
+++ SimpleControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class  SimpleControl: MonoBehaviour{
+
+    Animator miAnimator;
+
+    bool salto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        miAnimator = GetComponent<Animator>();
+    }
+
+    private void OnFire (InputValue movementValue)
+    {
+
+        Debug.Log("Estoy en la consola");
+       
+    }
+}
