568b091ccdb4bd07b5db184b253a1abf7e9ae13d
Davidguar83 <davidrguardiola@hotmail.es>
2021-01-31 22:39:14 +0100

    onfire

--- SimpleControl.cs
+++ SimpleControl.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 
 public class SimpleControl : MonoBehaviour
 {
@@ -16,12 +17,41 @@ public class SimpleControl : MonoBehaviour
     miAnimator = GetComponent<Animator>();
     }
 
-     private void OnFire (InputValue movementValue)
+    public void OnFire(InputAction.CallbackContext context)
     {
-        Debug.Log("Estoy en OnFire");
-        miAnimator.SetBool("salto", true);
+        switch (context.phase)
+        {
+            case InputActionPhase.Performed:
+
+                if(context.interaction is SlowTapInteraction)
+                {
+
+                    Debug.Log("tecla pulsada continuamente");
 
 
+                }
+                else
+                {
+
+                    Debug.Log("Salto efectuado");
+                  //  miAnimator.SetBool("Salto", false);
+                }
+                break;
+
+            case InputActionPhase.Started:
+
+                Debug.Log("Phase Iniciada");
+               // Salto();
+                break;
+
+            case InputActionPhase.Canceled:
+                Debug.Log("Phase Cancelada");
+                break;
+
+      //  Debug.Log("Estoy en OnFire");
+       // miAnimator.SetBool("salto", true);
+
+        }
     }
 
 }
